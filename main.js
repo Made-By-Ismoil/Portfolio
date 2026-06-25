d65c69f6a8f51f667c9b6cfa4d5a6f7c568d966b
ismoil <ismoilshoilyosov8@gmail.com>
2026-06-25 16:35:05 +0500

    Continue

---
+++ main.js
@@ -0,0 +1,8 @@
+let tg = document.querySelector(".footer__box-img-tg");
+let gmail = document.querySelector(".footer__box-img-gmail")
+tg.addEventListener("click", ()=>{
+    window.open("https://t.me/madebyismoil", "_blank")
+})
+gmail.addEventListener("click", ()=>{
+    window.location.href = "mailto:ismoilshoilyosov8@gmail.com"
+})
